d9f3f39f4923211abe74850f47d00d99d014e7ed
leslie helou <leslie@ccs-indy.com>
2024-02-06 11:02:26 -0500

    monitor file progress to/from afp/smb shares

---
+++ LocalDistributionPoint+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  LocalDistributionPoint+CoreDataClass.swift
+//  jamfcpr
+//
+//  Created by Leslie Helou on 1/27/24.
+//  Copyright © 2024 jamf. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(LocalDistributionPoint)
+public class LocalDistributionPoint: NSManagedObject {
+
+}
